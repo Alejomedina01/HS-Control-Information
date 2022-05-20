c3387b1c6daa76a1ef7491c457ecbd41198385e6
Yeison363RS <yehison363@gmail.com>
2022-05-20 17:00:18 -0500

    service y controllador para cliente

---
+++ src/main/java/com/hs/hscontrolinformation/controllers/ControllerClient.java
@@ -0,0 +1,43 @@
+package com.hs.hscontrolinformation.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.hs.hscontrolinformation.domain.Client;
+import com.hs.hscontrolinformation.services.ClientImplService;
+
+@Controller
+
+public class ControllerClient {
+
+    @Autowired
+    ClientImplService service ;
+
+    @GetMapping("/allClients")
+    public String listar() {
+        service.listar();
+        return "index";
+    }
+
+    @PostMapping("/saveClient")
+    public String guardar(Client data) {
+        service.guardar(data);
+        return "index";
+    }
+
+    @DeleteMapping("/deleteClient")
+    public String eliminar(Client data) {
+        service.eliminar(data);
+        return "index";
+    }
+
+    @GetMapping("/client/{idPersona}")
+    public String encontrar(Client data) {
+        service.encontrar(data);
+        return "index";
+    }
+}
---
+++ src/main/java/com/hs/hscontrolinformation/services/ClientImplService.java
@@ -0,0 +1,45 @@
+package com.hs.hscontrolinformation.services;
+
+import java.util.List;
+
+import com.hs.hscontrolinformation.dao.ClientDao;
+import com.hs.hscontrolinformation.util.ServiceTemplate;
+import com.hs.hscontrolinformation.domain.Client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientImplService implements ServiceTemplate<Client>{
+
+    @Autowired
+    ClientDao repository; 
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Client> listar() {
+        return repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void guardar(Client data) {
+        repository.save(data);
+    }
+
+    @Override
+    @Transactional(readOnly = false)
+    public void eliminar(Client data) {
+        repository.delete(data);
+        
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Client encontrar(Client data) {
+        repository.getById(data.getClientId());
+        return null;
+    }
+    
+}
