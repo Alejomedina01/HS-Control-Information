9747d0e4aaea32f38fbb88d579022606009fe7e8
Alejandro <edgar.medina01@uptc.edu.co>
2022-05-29 18:49:35 -0500

    MVC 1.0 Version Funcional!

--- src/main/java/com/hs/hscontrolinformation/controllers/ControllerClient.java
+++ src/main/java/com/hs/hscontrolinformation/controllers/ControllerClient.java
@@ -2,7 +2,6 @@ package com.hs.hscontrolinformation.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
