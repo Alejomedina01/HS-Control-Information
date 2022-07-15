f51bfe5cc6efae5d5238198f407ba66d4eb2fd55
Yeison363RS <yehison363@gmail.com>
2022-07-14 22:10:12 -0500

    pruebas funcionales listas

--- src/test/cypress/integration/FunctionalTesting.spec.js
+++ src/test/cypress/integration/FunctionalTesting.spec.js
@@ -115,7 +115,7 @@ describe('functional testing second sprint', function () {
         })
         cy.get('[test-id="id-contract"]').should('contain', IdContract)
     })
-    it.only('Agregar archivo PDF al contrato', () => {
+    it('Agregar archivo PDF al contrato', () => {
         cy.get('[test-id="contracts"]').click()
         cy.get('input[id="myInput"]').type('88888')
         cy.contains('88888').parent('tr').within(() => {
@@ -127,4 +127,100 @@ describe('functional testing second sprint', function () {
         cy.get('button[test-id="addFileContract"]').click()
         cy.get('tr[test-id="datas-fileContract"]').should('contain', "Contrato")
     })
+
+    it('Buscar y visualizar pdf asociado a un contrato', () => {
+        cy.get('[test-id="contracts"]').click()
+        cy.get('input[id="myInput"]').type('88888')
+        cy.contains('88888').parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Abrir').click()
+        })
+        cy.get('[test-id="visualizeDocuments"]').click()
+
+        cy.contains('Contrato').parent('tr').within(() => {
+            cy.get('td').eq(1).contains('a', 'Ver').click()
+        })
+        cy.get('[test-id="containerPdf"]')
+            .invoke('attr', 'src')
+            .then(src => {
+                cy.request(src).its('status').should('eq', 200);
+            });
+    })
+    it('Validacion de fechas de contrato (inicio,terminacion)', () => {
+        cy.get('[test-id="contracts"]').click()
+        cy.get('[test-id="addContract"]').click();
+        cy.get('input[id="idClient"]').type('33333')
+        cy.get('input[test-id="btnsearch"]').click()
+        cy.get('[test-id="confirmClient"]').click()
+        cy.get('input[name="idContract"]').type('88888')
+        cy.get('input[name="projectName"]').type('remodelacion restaurante')
+        cy.get('input[name="city"]').type('Tunja')
+        cy.get('input[name="contractDate"]').type('2022-08-21')
+        cy.get('#form-add-contract').within(() => {
+            cy.get('input[name="contractDate"]').then($el => $el[0].checkValidity()).should('be.false')
+        })
+        cy.get('input[name="initialValue"]').type('20000000')
+        cy.get('input[name="initialDateAct"]').type('2022-07-15')
+        cy.get('#form-add-contract').within(() => {
+            cy.get('input[name="initialDateAct"]').then($el => $el[0].checkValidity()).should('be.false')
+        })
+        cy.get('input[name="receivalDateAct"]').type('2022-05-15')
+        cy.get('#form-add-contract').within(() => {
+            cy.get('input[name="receivalDateAct"]').then($el => $el[0].checkValidity()).should('be.false')
+        })
+        cy.get('input[name="invoicedValue"]').type('10000000')
+        cy.get('input[name="warratyValue"]').type('200000')
+        cy.get('select[name="warrantyState"]').select('Sin pagar')
+        cy.get('select[name="contractState"]').select('En ejecucion')
+        cy.get('button[test-id="saveContract"]').click()
+    })
+    it('Validar fechas de trabajo de empleados ', () => {
+        cy.get('[test-id="contracts"]').click()
+        cy.get('input[id="myInput"]').type('88888')
+        cy.contains('88888').parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Abrir').click()
+        })
+        cy.get('[test-id="EmployeeContract"]').click()
+        cy.get('input[id="myInput"]').type('44444')
+        cy.contains('44444').parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Añadir').click()
+        })
+        cy.get('input[name="initialDate"]').type('2022-07-15')
+        cy.get('#form-vin-employ').within(() => {
+            cy.get('input[name="initialDate"]').then($el => $el[0].checkValidity()).should('be.false')
+        })
+        cy.get('input[name="finalDate"]').type('2022-06-15')
+        cy.get('#form-vin-employ').within(() => {
+            cy.get('input[name="finalDate"]').then($el => $el[0].checkValidity()).should('be.false')
+        })
+        cy.get('[test-id="saveEmployeeContract"]').click()
+    })
+    it.only('validar eliminacion de contratos, clientes, empleados', () => {
+        cy.get('[test-id="contracts"]').click()
+        cy.contains('88888').parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Abrir').click()
+        })
+        cy.get('[test-id="editContract"]').click()
+        cy.get('[test-id="deleteContract"]').should('not.exist');
+
+        cy.get('[test-id="home"]').click()
+        const idClient = "33333"
+        cy.get('[test-id="clients"]').click()
+        cy.get('input[id="myInput"]').type(idClient)
+        cy.contains(idClient).parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Abrir').click()
+        })
+        cy.get('[test-id="editClient"]').click()
+        cy.get('[test-id="deleteClient"]').should('not.exist');
+
+        cy.get('[test-id="home"]').click()
+        const idEmployee = '44444'
+        cy.get('[test-id="employees"]').click()
+        cy.get('input[id="myInput"]').type(idEmployee)
+        cy.contains(idEmployee).parent('tr').within(() => {
+            cy.get('td').eq(4).contains('a', 'Abrir').click()
+        })
+        cy.get('[test-id="editEmployee"]').click()
+        cy.get('[test-id="deleteEmployee"]').should('not.exist');
+    })
+
 })
