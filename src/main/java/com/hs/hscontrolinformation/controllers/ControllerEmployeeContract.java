1c0eff779926fb4a24c51d939ebb0ef3f68f7418
Yeison363RS <yehison363@gmail.com>
2022-07-29 12:43:14 -0500

    cambios en las busqueda con paginacion

--- src/main/java/com/hs/hscontrolinformation/controllers/ControllerEmployeeContract.java
+++ src/main/java/com/hs/hscontrolinformation/controllers/ControllerEmployeeContract.java
@@ -1,12 +1,15 @@
 package com.hs.hscontrolinformation.controllers;
 
+import com.hs.hscontrolinformation.domain.Client;
 import com.hs.hscontrolinformation.domain.Contract;
+import com.hs.hscontrolinformation.domain.Employee;
 import com.hs.hscontrolinformation.domain.EmployeeContract;
 import com.hs.hscontrolinformation.services.ContractServiceImp;
 import com.hs.hscontrolinformation.services.EmployeeContractServiceImp;
 import com.hs.hscontrolinformation.services.EmployeeServiceImp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 @Slf4j
@@ -33,16 +37,38 @@ public class ControllerEmployeeContract {
 
     private Long idEmployeeActual;
 
-    @GetMapping("/addEmployeeToContract/{idContract}")
+    /*@GetMapping("/addEmployeeToContract/{idContract}")
     public String asociateEmployeeContract(Contract contract, Model model){
         contract = (Contract) contractServiceImp.find(contract);
         model.addAttribute("contract", contract);
-        var employees = ecServiceImp.getEmployeesNotAsociated(contract.getIdContract());
+        var employees = ecServiceImp.findPage(contract.getIdContract());
         model.addAttribute("employees", employees);
         log.info("e - " + employees);
         return "asociateEC";
+    }*/
+    @GetMapping("/addEmployeeToContract/{idContract}")
+    public String findClienForContract(Model model,@PathVariable("idContract") String idContract) {
+        return getOnePageClients(model,1,idContract,null);
     }
-
+    @GetMapping("/addEmployeeToContract/page/{pageNumber}/{idContract}")
+    public String getOnePageClients(Model model, @PathVariable("pageNumber") int currentPage,@PathVariable("idContract") String idContract,String myInput) {
+        Contract contract = contractServiceImp.findById(idContract);
+        model.addAttribute("contract", contract);
+        MyPage<String> page=null;
+        if (myInput == null || myInput.isEmpty()){
+            page=ecServiceImp.findPage(idContract,currentPage);
+            myInput="";
+        }else{
+            page = ecServiceImp.findByKeyword(idContract,myInput);
+            currentPage=1;
+        }
+        model.addAttribute("keyWordSearch",myInput);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalPages", page.getNumberPages());
+        model.addAttribute("totalItems", page.getTotalItems());
+        model.addAttribute("employees", page.getContent());
+        return "asociateEC";
+    }
     @GetMapping("/crearAsociacion/{idContract}/{idEmployee}")
     public String createAsociation(@PathVariable String idContract, @PathVariable String idEmployee, Model model){
         idContractActual = idContract;
@@ -63,7 +89,6 @@ public class ControllerEmployeeContract {
     public String uploadAsociation(EmployeeContract employeeContract, RedirectAttributes redirectAttrs){
         employeeContract.setContract(contractServiceImp.findById(this.idContractActual));
         employeeContract.setEmployee(employeeService.findById(this.idEmployeeActual));
-        log.info("contrato cliente - " + this.idContractActual + " - " + this.idEmployeeActual);
         ecServiceImp.save(employeeContract);
         redirectAttrs.addFlashAttribute("mensaje", "✓ Empleado Asociado al Contrato")
                 .addFlashAttribute("clase", "success");
--- src/main/java/com/hs/hscontrolinformation/services/EmployeeContractServiceImp.java
+++ src/main/java/com/hs/hscontrolinformation/services/EmployeeContractServiceImp.java
@@ -1,6 +1,8 @@
 package com.hs.hscontrolinformation.services;
 
+import com.hs.hscontrolinformation.controllers.MyPage;
 import com.hs.hscontrolinformation.dao.EmployeeContractDao;
+import com.hs.hscontrolinformation.dao.EmployeeDao;
 import com.hs.hscontrolinformation.domain.Employee;
 import com.hs.hscontrolinformation.domain.EmployeeContract;
 import com.hs.hscontrolinformation.util.ServiceTemplate;
@@ -15,7 +17,8 @@ public class EmployeeContractServiceImp implements ServiceTemplate<EmployeeContr
 
     @Autowired
     private EmployeeContractDao employeeContractDao;
-
+    @Autowired
+    private EmployeeDao employeeDao;
     @Override
     public List<EmployeeContract> list() {
         return employeeContractDao.findAll();
@@ -41,16 +44,31 @@ public class EmployeeContractServiceImp implements ServiceTemplate<EmployeeContr
         return null;
     }
 
+    private static final   double maxItems=4;
+
     @Transactional(readOnly = true)
     public EmployeeContract find(EmployeeContract employeeContract) {
         return employeeContractDao.findById(employeeContract.getIdEmployeeContract()).orElse(null);
     }
-
     @Transactional(readOnly = true)
-    public List<String> getEmployeesNotAsociated(String idContract){
-        return this.employeeContractDao.getEmployeesNotAsociated(idContract);
+    public MyPage<String> findPage(String idContract,int pageNumber){
+        MyPage<String> myPage=new MyPage<>();
+        long total = employeeDao.count()-employeeContractDao.count();
+        List<String> employees=employeeContractDao.getEmployeesNotAsociated(idContract,(long)((pageNumber-1)*maxItems) , (int) maxItems);
+        myPage.setTotalItems(total);
+        myPage.setNumberPages((int) Math.ceil(total/maxItems));
+        myPage.setContent(employees);
+        return myPage;
     }
-
+    public MyPage<String> findByKeyword(String idContract,String keyword){
+        MyPage<String> myPage=new MyPage<>();
+        long total = employeeDao.count()-employeeContractDao.count();
+        myPage.setTotalItems(total);
+        List<String> employees=employeeContractDao.findAllByKeyWord(idContract,"%"+keyword.toLowerCase()+"%");
+        myPage.setNumberPages(1);
+        myPage.setContent(employees);
+        return myPage;
+    }
     @Transactional(readOnly = false)
     public void deleteAsociation(String idEmployee, String idContract){
         this.employeeContractDao.deleteAsociation(Long.parseLong(idEmployee), idContract);
